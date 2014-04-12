033721a3cee16f51c940bf9d2472e7f02544a43d
Sergei Trofimovich <slyfox@gentoo.org>
2014-04-12 14:43:05 +0300

    run_chroot.sh: unshare only mount for now
    All the rest is likely disabled on various boxes.
    
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- scripts/run_chroot.sh
+++ scripts/run_chroot.sh
@@ -2,8 +2,7 @@
 
 script_base=gentoo-chrootiez
 
-# fault-propagation wise --mount is good-enough
-exec unshare --fork --mount --ipc --pid --uts \
+exec unshare --mount \
      "$script_base"/scripts/run_chroot_unshared.sh "$@"
 
 echo "WARNING: unshare not found, only chroot facility will be used"

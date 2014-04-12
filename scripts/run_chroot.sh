acf5f60abf576170ac6c1c748332dadc81c04f0b
Sergei Trofimovich <slyfox@gentoo.org>
2014-04-12 14:09:36 +0300

    scripts/run_chroot.sh: perform less forking
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- scripts/run_chroot.sh
+++ scripts/run_chroot.sh
@@ -3,6 +3,8 @@
 script_base=gentoo-chrootiez
 
 # fault-propagation wise --mount is good-enough
-exec unshare --fork --mount --ipc --pid --uts "$script_base"/scripts/run_chroot_unshared.sh "$@"
+exec unshare --fork --mount --ipc --pid --uts \
+     "$script_base"/scripts/run_chroot_unshared.sh "$@"
+
 echo "WARNING: unshare not found, only chroot facility will be used"
-                                              "$script_base"/scripts/run_chroot_unshared.sh "$@"
+exec "$script_base"/scripts/run_chroot_unshared.sh "$@"

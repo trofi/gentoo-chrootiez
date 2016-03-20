d3b5e5bf187b69a1ddb7cd6d1c8744a9d5de7fa7
Sergei Trofimovich <siarheit@google.com>
2016-03-20 20:19:15 +0000

    make_typical_confs.sh: added a --makeopts= knob to limit parallelism
    Useful for machines with lots of CPUs but small amount of RAM.
    My alpha friend has 8GB and 4 CPUs. That occasionally OOMs GHC.
    
    Also allows workarounding parallel build bugs.
    
    Signed-off-by: Sergei Trofimovich <siarheit@google.com>

--- bound/make_typical_confs.sh
+++ bound/make_typical_confs.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 
+makeopts=auto
+
+for a; do
+    case "$a" in
+        --makeopts=*)
+            makeopts=${a#--makeopts=}
+            ;;
+        *)
+            die "unknown option: $a"
+            ;;
+    esac
+done
+
+if [ "${makeopts}" = auto ]; then
+    makeopts=$(expr `nproc` + 1)
+fi
+
 cat >conf/make.conf.local <<EOF
-MAKEOPTS=-j$(expr `nproc` + 1)
+MAKEOPTS=-j${makeopts}
 FEATURES="\${FEATURES} test"
 EOF
